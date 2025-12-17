6b3949347a419fea2393c1723a9247e3fabe6cf7
Zerrant2 <Kao242003@gmail.com>
2025-12-17 23:20:57 +0300

    My first commit :)

--- src/TextDataReader.py
+++ src/TextDataReader.py
@@ -17,4 +17,4 @@ class TextDataReader(DataReader):
                     subj, score = line.split(":", maxsplit=1)
                     self.students[self.key].append(
                         (subj.strip(), int(score.strip())))
-                return self.students
+        return self.students  # <--- ИСПРАВЛЕНИЕ: return должен быть на одном уровне с with
